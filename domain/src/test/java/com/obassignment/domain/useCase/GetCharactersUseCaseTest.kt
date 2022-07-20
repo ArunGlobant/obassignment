a4aac1e189d4230082ae92ca7cd61067347460dc
arun.pandey <arun.pandey@globant.com>
2022-07-20 16:30:43 +0530

    blank class

--- domain/src/test/java/com/obassignment/domain/useCase/GetCharactersUseCaseTest.kt
+++ domain/src/test/java/com/obassignment/domain/useCase/GetCharactersUseCaseTest.kt
@@ -14,37 +14,4 @@ import org.mockito.Mock
 
 class GetCharactersUseCaseTest{
 
-    @Mock
-    lateinit var charactersRepository: CharactersRepository
-    lateinit var underTest: GetCharactersUseCase
-
-    @Before
-    fun setUp(){
-        MockKAnnotations.init(this)
-        underTest = GetCharactersUseCase(charactersRepository)
-    }
-    @Test
-    fun `ensure getting success in response`(){
-        val thumbnailModel = ThumbnailModel("jpg", "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784")
-        val resultModel = ResultModel(1, "3-D Man", thumbnailModel)
-        val characterList = listOf(resultModel)
-
-        assertEquals(true,  underTest.invoke())
-        verify { (Resource.Success(characterList)) }
-    }
-
-    @Test
-    fun `ensure getting null in response`(){
-        val characterList = listOf(null)
-        assertEquals(true,  underTest.invoke())
-        verify { (Resource.Success(characterList)) }
-    }
-    @Test
-    fun `ensure getting blank in response`(){
-        val thumbnailModel = ThumbnailModel("", "")
-        val resultModel = ResultModel(0,"",thumbnailModel)
-        val characterList = listOf(null)
-        assertEquals(true,  underTest.invoke())
-        verify { (Resource.Success(characterList)) }
-    }
 }
